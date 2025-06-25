ad1655b4e6316bd5e3086d15a6baa57205e58001
ehab <ehab@hazen.ai>
2025-06-25 19:22:26 +0500

    feat: implement retries to open stream

--- benchmark.cc
+++ benchmark.cc
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <future>
 #include <memory>
+#include <opencv2/core/base.hpp>
 #include <sys/types.h>
 #include <thread>
 #include <unistd.h>
@@ -11,14 +12,28 @@
 
 template <typename T> using vec = std::vector<T>;
 template <typename T> using fut = std::future<T>;
+template <typename T> using up = std::unique_ptr<T>;
 template <typename T> using sp = std::shared_ptr<T>;
+using u64 = uint64_t;
 
-constexpr int FRAME_COUNT = 200;
+constexpr int FRAME_COUNT = 2000;
 
-int ReadNFrames(StreamHandler &stream_handler, int frame_count) {
+// TODO: if a task fails, the others should take on its work
+
+up<StreamHandler> OpenStream(const std::string &uri, int retry_count = 3) {
+  for (int i = 0; i < retry_count; ++i) {
+    up<StreamHandler> stream_handler = std::make_unique<StreamHandler>(uri);
+    if (stream_handler->IsStreamOpen()) {
+      return stream_handler;
+    }
+  }
+  return nullptr;
+}
+
+u64 ReadNFrames(up<StreamHandler> stream_handler, int frame_count) {
   ino64_t read_byte_count = 0;
-  for (int i = 0; i < frame_count && stream_handler.IsStreamOpen(); ++i) {
-    vec<uint8_t> bytes = stream_handler.PullSample();
+  for (int i = 0; i < frame_count && stream_handler->IsStreamOpen(); ++i) {
+    vec<uint8_t> bytes = stream_handler->PullSample();
     read_byte_count += bytes.size();
   }
   return read_byte_count;
@@ -26,21 +41,28 @@ int ReadNFrames(StreamHandler &stream_handler, int frame_count) {
 
 void MultipleStreamsMultipleReaders(int frame_count, int stream_count) {
   vec<fut<void>> tasks;
-  int base = frame_count / stream_count;
-  int remainder = frame_count % stream_count;
+  int base = frame_count / stream_count, remainder = frame_count % stream_count;
 
   for (int i = 0; i < stream_count; ++i) {
     int task_frame_count = base + (i < remainder ? 1 : 0);
-    tasks.push_back(
-        std::async(std::launch::async, [i, frame_count = task_frame_count]() {
-          StreamHandler stream_handler("rtsp://10.1.10.250:555/AlHajj01.mp4");
-          std::cout << i << " Read " << ReadNFrames(stream_handler, frame_count)
-                    << "/"
-                    << frame_count * stream_handler.GetStreamWidth() *
-                           stream_handler.GetStreamHeight() * 3
-                    << " bytes\n";
-        }));
+    auto task = [i, frame_count = task_frame_count]() {
+      up<StreamHandler> stream_handler =
+          OpenStream("rtsp://10.1.10.250:555/AlHajj01.mp4");
+      if (stream_handler == nullptr) {
+        std::cerr << i << "Failed to open stream\n";
+        return;
+      }
 
+      int width = stream_handler->GetStreamWidth(),
+          height = stream_handler->GetStreamHeight();
+      u64 read_frame_count =
+          ReadNFrames(std::move(stream_handler), frame_count);
+
+      std::cout << i << " Read " << read_frame_count << "/"
+                << u64(frame_count) * width * height * 3 << " bytes\n";
+    };
+
+    tasks.push_back(std::async(std::launch::async, task));
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 
